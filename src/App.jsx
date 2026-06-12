149a7d425fcef109bd4390814bdf50b365878f80
GiovannaCarraro <giovanna.c.oliveira9@aluno.senai.br>
2026-06-12 08:51:54 -0300

    Faendo uma funçao pro contador

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,7 @@ function App(){
 
 return (
 <>
-{/* <UseStateHook />
-<Contador contador = {}/> */}
+<UseStateHook />
 </>
 
 );
--- src/components/UseStateHook/UseStateHook.jsx
+++ src/components/UseStateHook/UseStateHook.jsx
@@ -6,9 +6,33 @@ function UseStateHook(){
 
     const [contador, setContador] = useState(0);
 
+    const Aumentar = () => {
+        setContador(contador + 1);
+    };
+
+    const Diminuir = () => {
+        setContador(contador - 1);
+    };
+
+    const Zerar = () => {
+        setContador(0);
+    };
+
+    const Sortear = () => {
+        setContador(Math.floor(Math.random() * 100));
+    };
+
+    function FormatarContador() {
+    if (contador < 10 && contador >= 0) {
+        return `0${contador}`;
+    }
+
+    return contador;
+}
+
     return(
         <div className={styles.container}>
-           <h1>{contador}</h1>
+           <h1>{FormatarContador()}</h1>
 
             <button onClick={Aumentar}>+</button>
 
